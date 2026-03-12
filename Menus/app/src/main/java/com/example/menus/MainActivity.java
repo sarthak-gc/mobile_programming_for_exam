1445de3a018f3b25f1659c07b9cc553b7f3ab11f
sarthak-gc <sarthak-gc@users.noreply.github.com>
2026-03-13 00:12:53 +0545

    menu, submenu, group(check) menu

---
+++ Menus/app/src/main/java/com/example/menus/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.menus;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.activity_main);
+    }
+}
